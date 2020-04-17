5deca6f4533ef8a1c10b9bbf9db1739b841020cc
Pedro Young <pedroyoung@livenation.com>
2020-04-16 21:09:50 -0700

    Updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 /* eslint-disable no-console */
 import './styles.css'
 
-const form = document.getElementById('infoForm')
+const inputNodeList = document
+  .getElementById('infoForm')
+  .querySelectorAll('select, input')
 
-console.info('form', form)
+console.info('inputNodeList', inputNodeList)
+
+const readNodeList = nodeList => {
+  for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].style.backgroundColor = 'darkblue'
+  }
+}
+
+const test = readNodeList(inputNodeList)
+
+console.log('test', test)
+
+export default readNodeList
