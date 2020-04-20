10e8e047f706cc2612d96954ead664feb2ebfabc
Pedro Young <pedroyoung@livenation.com>
2020-04-19 17:49:57 -0700

    Updates

--- src/form/constraints.js
+++ src/form/constraints.js
@@ -4,6 +4,11 @@ const constraints = {
     length: {
       minimum: 5,
       message: 'must be at least 6 characters.'
+    },
+    format: {
+      pattern: '[a-z0-9]+',
+      flags: 'i',
+      message: 'can only contain alphanumeric characters'
     }
   },
   busAddressLine1: {
@@ -11,6 +16,51 @@ const constraints = {
     length: {
       minimum: 4,
       message: 'must be at least 4 characters.'
+    }
+  },
+  busCity: {
+    presence: true,
+    length: {
+      minimum: 4,
+      message: 'must be at least 4 characters.'
+    }
+  },
+  busState: {
+    presence: true,
+    length: {
+      minimum: 2,
+      message: 'must be at least 4 characters.'
+    }
+  },
+  busPostalCode: {
+    presence: true,
+    length: {
+      minimum: 5,
+      message: 'must be 5 digits.'
+    },
+    format: {
+      pattern: '[0-9]+',
+      flags: 'i',
+      message: 'can only contain 0-9'
+    }
+  },
+  busCountry: {
+    presence: true,
+    length: {
+      minimum: 4,
+      message: 'must be at least 4 characters.'
+    }
+  },
+  busTin: {
+    presence: true,
+    length: {
+      minimum: 9,
+      message: 'must be 9 digits.'
+    },
+    format: {
+      pattern: '[0-9]+',
+      flags: 'i',
+      message: '(Tax ID) can only contain 0-9'
     }
   }
 }
